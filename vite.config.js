8e6b1d046be9f26b45cbac3a0bbef1b04512b770
Edgard Carrion <edgard.jonthcs@gmail.com>
2024-04-26 19:57:55 -0400

    se realizan cambios en vite.config.js se agrega propiedad base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base:'https://EdgardJonh.github.io/Desafio_React_Router1/'
+   base:'https://EdgardJonh.github.io/Desafio_React_Router1/'
 })
